4f98a47ad7a8092bf563f0714aec4a666221af0d
Elvis <elvis.ciotti@digital.justice.gov.uk>
2014-11-26 12:37:26 +0000

    Update MockeryStubTest.php

--- test/MockeryStubTest.php
+++ test/MockeryStubTest.php
@@ -11,7 +11,7 @@ class MockeryStubTest extends PHPUnit_Framework_TestCase
             'foo(1)' => '[2]',
             'foo(2)' => '[3]',
             'foo(2.5,2.6)' => '[4]',
-            'foo(3)->foo(4)' => '[5]',
+            'foo(3)->foo(4,)' => '[5]',
             'getServiceLocator()->get(Doctrine\ORM\EntityManager)->getRepository(User)->find(1)' => '[6]'
         ));
         
@@ -20,7 +20,7 @@ class MockeryStubTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('[2]', $stub->foo(1) ); // no difference with string or integer
         $this->assertEquals('[3]', $stub->foo('2') );
         $this->assertEquals('[4]', $stub->foo('2.5', 2.6) );
-        $this->assertEquals('[5]', $stub->foo('3')->foo('4') );
+        $this->assertEquals('[5]', $stub->foo('3')->foo('4', null) );
         $this->assertEquals('[6]', $stub->getServiceLocator()->get('Doctrine\ORM\EntityManager')->getRepository('User')->find(1));
     }
 }
